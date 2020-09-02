7b1625427bab9b9744a08ebacae3f3bb6ea22c53
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-02 22:59:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,11 @@
 class TicTacToe
-
+  #assigns an instance variable @board to an array with 9 blank spaces " "
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
   WIN_COMBINATIONS = [
+    #WIN_COMBINATIONS with arrays for each win combination
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -11,11 +16,7 @@ class TicTacToe
     [6, 4, 2]
   ]
 
-def initialize
-  @board = Array.new(9, " ")
-end
-
-def display_board
+def display_board #prints arbitrary arrangements of the board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -26,13 +27,13 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
-
+#allows "X" player in the top left and "O" in the middle
 def move(index, current_player = "X")
-  @board[index] = current_player
+   @board[index] = current_player
 end
 
 def position_taken?(index)
-  !(@board[index].nil? || @board[index] == " ")
+   !(@board[index].nil? || @board[index] == " ")
 end
 
 def valid_move?(index)
@@ -50,8 +51,7 @@ def turn_count
 end
 
 def current_player
-  #if the turn count is an even number, that means O just went, so the next/current player is X
-  num_turns = turn_count
+    num_turns = turn_count
   if num_turns % 2 == 0
     player = "X"
   else
